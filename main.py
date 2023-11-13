e060f13a32c6005a94781fd31438992e0684d784
Philippe LUU <philippemtp34@gmail.com>
2023-11-13 10:58:51 +0100

    Création fenetre base

---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import simpledialog
+from PIL import Image, ImageTk
+
+class WishPacketTracer:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Wish Packet Tracer")
+
+        self.canvas = tk.Canvas(root, bg="ivory", width=800, height=600)
+        self.canvas.pack(expand=tk.YES, fill=tk.BOTH)
+
+        self.items = []  # Liste pour stocker les éléments dessinés
+        self.current_item = None  # Stocke l'élément actuellement sélectionné
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = WishPacketTracer(root)
+    root.mainloop()
